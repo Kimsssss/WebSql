67a923a0939ef025eee34cb173b76f9beecd2819
김현수 <ekekejek@naver.com>
2015-06-15 10:57:37 +0900

    DTO셋팅완료

--- WebSQL/src/main/java/com/sqlweb/dto/Board_A_DTO.java
+++ WebSQL/src/main/java/com/sqlweb/dto/Board_A_DTO.java
@@ -1,5 +1,45 @@
 package com.sqlweb.dto;
 
+import java.sql.Date;
+
 public class Board_A_DTO {
 
+	private int board_a_id;
+	private String board_a_title;
+	private String board_a_content;
+	private Date board_a_date;
+	private String user_id;
+	public int getBoard_a_id() {
+		return board_a_id;
+	}
+	public void setBoard_a_id(int board_a_id) {
+		this.board_a_id = board_a_id;
+	}
+	public String getBoard_a_title() {
+		return board_a_title;
+	}
+	public void setBoard_a_title(String board_a_title) {
+		this.board_a_title = board_a_title;
+	}
+	public String getBoard_a_content() {
+		return board_a_content;
+	}
+	public void setBoard_a_content(String board_a_content) {
+		this.board_a_content = board_a_content;
+	}
+	public Date getBoard_a_date() {
+		return board_a_date;
+	}
+	public void setBoard_a_date(Date board_a_date) {
+		this.board_a_date = board_a_date;
+	}
+	public String getUser_id() {
+		return user_id;
+	}
+	public void setUser_id(String user_id) {
+		this.user_id = user_id;
+	}
+	
+	
+	
 }
--- WebSQL/src/main/java/com/sqlweb/dto/Board_P_DTO.java
+++ WebSQL/src/main/java/com/sqlweb/dto/Board_P_DTO.java
@@ -1,5 +1,45 @@
 package com.sqlweb.dto;
 
-public class Board_P_DTO {
+import java.sql.Date;
 
+public class Board_P_DTO {
+	
+	private int board_p_id;
+	private String board_p_title;
+	private String board_p_content;
+	private Date board_p_date;
+	private String user_id;
+	public int getBoard_p_id() {
+		return board_p_id;
+	}
+	public void setBoard_p_id(int board_p_id) {
+		this.board_p_id = board_p_id;
+	}
+	public String getBoard_p_title() {
+		return board_p_title;
+	}
+	public void setBoard_p_title(String board_p_title) {
+		this.board_p_title = board_p_title;
+	}
+	public String getBoard_p_content() {
+		return board_p_content;
+	}
+	public void setBoard_p_content(String board_p_content) {
+		this.board_p_content = board_p_content;
+	}
+	public Date getBoard_p_date() {
+		return board_p_date;
+	}
+	public void setBoard_p_date(Date board_p_date) {
+		this.board_p_date = board_p_date;
+	}
+	public String getUser_id() {
+		return user_id;
+	}
+	public void setUser_id(String user_id) {
+		this.user_id = user_id;
+	}
+
+	
+	
 }
