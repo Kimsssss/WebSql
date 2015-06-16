0ded6bda57cd059d9d7793eaef14a941e7933f2f
khg <khg>
2015-06-16 14:21:50 +0900

    board UI

--- WebSQL/src/main/java/com/sqlweb/controller/BoardController.java
+++ WebSQL/src/main/java/com/sqlweb/controller/BoardController.java
@@ -1,8 +1,16 @@
 package com.sqlweb.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+
 public class BoardController {
 
+	@RequestMapping(value="board.html")
+	public String Boardwrite(){
+		
+		return "Boardwrite";
+	}
+	
 }
