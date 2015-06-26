575d80c7d1e2914b7d75ff5397421e64bd2a7b11
Seulmin <crazy6798@naver.com>
2015-06-26 16:41:26 +0900

    123

--- WebSQL/src/main/java/com/sqlweb/controller/DMLController.java
+++ WebSQL/src/main/java/com/sqlweb/controller/DMLController.java
@@ -2,10 +2,16 @@ package com.sqlweb.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class DMLController {
 
+	@RequestMapping(value="/dml.html", method=RequestMethod.GET)
+	public String test(){
+		System.out.println("DML controller");
+		return "DML";
+	}
 	/*@RequestMapping
 	public void Select(){
 		
